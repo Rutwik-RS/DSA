b95ef1c9d090c95c71ef559a7cbec60d89549343
Rutwik-RS <rutwikrs1006@gmail.com>
2026-08-01 14:13:04 +0530

    Time: 1 ms (51.87%), Space: 42.9 MB (41.06%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean predictTheWinner(int[] a) {
+        int n = a.length;
+        int[][] dp = new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            dp[i][i] = a[i];
+        }       
+        for(int len = 2;len<=n;len++)
+        {
+            for(int l = 0;l+len-1 < n;l++)
+            {
+                int r = l + len-1;
+                int left = a[l] - dp[l+1][r];
+                int right = a[r] - dp[l][r-1];
+                dp[l][r] = Math.max(left,right);
+            }
+        }
+        if(dp[0][n-1] >= 0) return true;
+        return false;
+    }
+}
